836afd8802acf6c91365fd8d1ee87d370607d1f1
Michaelbrima <157252258+Michaelbrima@users.noreply.github.com>
2024-09-18 23:29:58 -0400

    Update index.php
    Added CSS styles and Bootstrap CSS formatting.

--- api/index.php
+++ api/index.php
@@ -2,14 +2,19 @@
 <html>
     <head>
         <title>Computer System Database</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	<link href="index.css" rel="stylesheet">    
     </head>
 
 <body>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+<div id='top'>
     <h1 align="center"> Computer System Database </h1>
 <p align="center"> <b> Browse for your ideal computer system by filtering through multiple attributes.</b></p> 
 
 <p align="center"> (Please click on one of the filter buttons below to begin using the database.) </p>
-
+</div>
 <br>
 	  
 	  <table border=\"5\" cellpadding=\"5\" cellspacing=\"0\" style=\"border-  collapse: collapse\" bordercolor=\"#808080\" width=\"100&#37;\"    id=\"AutoNumber2\" >
