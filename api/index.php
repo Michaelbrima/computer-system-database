a2dfacb47b13a30f339ae995297c3eb00dbfde8a
Michaelbrima <157252258+Michaelbrima@users.noreply.github.com>
2024-09-21 02:51:52 -0400

    Update index.php

--- api/index.php
+++ api/index.php
@@ -13,7 +13,7 @@
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 <div id='top'>
-    <h1 align="center"> Computer System Database</h1>
+    <h1 align="center"> Computer System Database DOS</h1>
 <p align="center"> <b> Browse for your ideal computer system by filtering through multiple attributes.</b></p> 
 
 <p align="center"> (Please click on one of the filter buttons below to begin using the database.) </p>
